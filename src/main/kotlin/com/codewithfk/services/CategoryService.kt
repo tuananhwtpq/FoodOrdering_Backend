3b703f72ab5b2576158752b5d8021a437d058430
tuananhwtpq <tuananhwtpq@gmail.com>
2025-08-23 17:41:13 +0700

    thêm danh mục, xóa danh mục, lấy tất car danh mục

--- src/main/kotlin/com/codewithfk/services/CategoryService.kt
+++ src/main/kotlin/com/codewithfk/services/CategoryService.kt
@@ -1,8 +1,12 @@
 package com.codewithfk.services
 
 import com.codewithfk.database.CategoriesTable
+import com.codewithfk.database.MenuItemsTable
+import com.codewithfk.database.RestaurantsTable
 import com.codewithfk.model.Category
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.inList
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.UUID
 
@@ -28,11 +32,32 @@ object CategoryService {
     /**
      * Add a new category (admin-only functionality).
      */
-    fun addCategory(name: String): UUID {
+    fun addCategory(name: String, imageUrl: String): UUID {
         return transaction {
             CategoriesTable.insert {
                 it[this.name] = name
+                it[this.imageUrl] = imageUrl
             } get CategoriesTable.id
         }
     }
+
+    //Xóa 1 danh mục theo ID
+    fun deleteCategory(id: UUID): Boolean {
+        return transaction {
+            val restaurantIds = RestaurantsTable
+                .select { RestaurantsTable.categoryId eq id }
+                .map { it[RestaurantsTable.id] }
+
+            if (restaurantIds.isNotEmpty()) {
+                MenuItemsTable.deleteWhere { MenuItemsTable.restaurantId inList restaurantIds }
+
+                RestaurantsTable.deleteWhere { RestaurantsTable.categoryId eq id }
+            }
+
+            val deletedRows = CategoriesTable.deleteWhere { CategoriesTable.id eq id }
+
+            deletedRows > 0
+        }
+    }
+
 }
